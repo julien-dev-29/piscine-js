92c083a2a0af5250637b91c31daddd852d2aa1b1
jurol <rollandjulien718@gmail.com>
2026-02-20 10:01:43 +0100

    Finish loop exercices

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Training Exercices
+
+## Point à bosser: recursivité, dates
+
+```js
+console.log();
+```
+
+- Point 1
+- Point 2
+- Point 3
--- loop/sums.js
+++ loop/sums.js
@@ -1,11 +1,22 @@
 /**
- * 
- * @param {number} number 
+ *
+ * @param {number} number
  */
 const sums = (number) => {
-    
-}
-
-
+  const result = [];
+  const backtrack = (remaining, start, path) => {
+    if (remaining === 0) {
+      result.push([...path]);
+      return;
+    }
+    for (let i = start; i <= remaining; i++) {
+      path.push(i);
+      backtrack(remaining - i, i, path);
+      path.pop();
+    }
+  };
+  backtrack(number, 1, []);
+  return result;
+};
 
 console.log(sums(4));

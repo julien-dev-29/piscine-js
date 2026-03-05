8d9e2b9b65ed653d831543aa195b02be57db1482
jurol <rollandjulien718@gmail.com>
2026-03-05 19:29:08 +0100

    Finish pronoun exercice

--- object/pronoun.js
+++ object/pronoun.js
@@ -6,18 +6,19 @@ const PRONOUNS = ["i", "you", "he", "she", "it", "they", "we"];
  */
 const pronoun = (str) => {
   const result = {};
-  const words = str.match(/\b\w+\b/gi);
+  const words = str.toLowerCase().match(/\b\w+\b/g);
+  console.log(words);
   for (let i = 0; i < words.length; i++) {
     const word = words[i];
     if (PRONOUNS.includes(word)) {
       if (!result[word]) {
         result[word] = { word: [], count: 0 };
       }
-      result[word].count++;
       const next = words[i + 1];
       if (next && !PRONOUNS.includes(next)) {
         result[word].word.push(next);
       }
+      result[word].count++;
     }
   }
   return result;
